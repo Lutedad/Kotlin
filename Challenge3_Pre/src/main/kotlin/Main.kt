1e8f174dbdce2dac61dafd65d47a04ea5a15952b
Junu_Choi_laptop <a01081270127@gmail.com>
2023-04-25 12:38:05 -0700

    function addition is on progress

--- Challenge3_Pre/src/main/kotlin/Main.kt
+++ Challenge3_Pre/src/main/kotlin/Main.kt
@@ -1,4 +1,31 @@
-fun main(array: Array<String>){
-    println("Test")
-    //After Pushing
+private operator fun String?.contains(any: Any): Boolean {
+
+}
+
+fun main(array: Array<String>) {
+    println("Activating")
+    Cal().addition()
+}
+
+class Cal() {
+    fun addition(numbers: Array<Int>): Int {
+        var addResult = 0
+        for (number in numbers) {
+            addResult += number
+        }
+        return addResult
+    }
+
+    fun subtraction(numbers: Array<Int>): Int {
+        var subResult = 0
+        for (number in numbers)
+            subResult -= number
+        return subResult
+    }
+
+    fun division(numbers: Array<Int>) {
+    }
+
+    fun multiplication(numbers: Array<Int>) {
+    }
 }
