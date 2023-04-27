99595cad29ca11f09b145b0be74c2d85c9aa0bba
Junu_Choi_laptop <a01081270127@gmail.com>
2023-04-26 17:41:11 -0700

    on progress

---
+++ Challenge_10/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+// 두 개의 정수 리스트를 받아서, 두 리스트의 교집합을 반환하는 함수를 작성하세요.
+// 함수명: intersection
+// 매개변수: 리스트 a, 리스트 b
+// 반환값: 두 리스트의 교집합을 담은 리스트
+
+// 예시
+// intersection(listOf(1, 2, 3), listOf(2, 3, 4)) -> [2, 3]
+
+fun main(){
+
+}
+
+fun intersection(a : List<Int>,b : List<Int>){
+    a.forEach { b.forEach(it) }
+}
